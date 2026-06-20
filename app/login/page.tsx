70db57ef56bb2622d410800fd9837361bd0789b7
Hashirahmed2982 <hashirahmed@sabanciuniv.edu>
2026-06-21 03:37:28 +0500

    d

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -22,10 +22,13 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
+      
       // const response = await fetch(
       //   `${process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000"}/api/v1/auth/login`,
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL || "http://178.104.162.74:5000"}/api/v1/auth/login`,
+      // const response = await fetch(
+      //   `${process.env.NEXT_PUBLIC_API_URL || "http://178.104.162.74:5000"}/api/v1/auth/login`,
+       const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL || "http://167.233.51.100:5000"}/api/v1/auth/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
