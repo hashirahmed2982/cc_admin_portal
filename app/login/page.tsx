1437fcee52ed983c970e880c7588ab41108c3296
Hashirahmed2982 <hashirahmed@sabanciuniv.edu>
2026-06-22 02:50:38 +0500

    d

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -28,7 +28,7 @@ export default function LoginPage() {
       // const response = await fetch(
       //   `${process.env.NEXT_PUBLIC_API_URL || "http://178.104.162.74:5000"}/api/v1/auth/login`,
        const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL || "http://167.233.51.100:5000"}/api/v1/auth/login`,
+        `${process.env.NEXT_PUBLIC_API_URL || "https://api.cardcove.com"}/api/v1/auth/login`,
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
